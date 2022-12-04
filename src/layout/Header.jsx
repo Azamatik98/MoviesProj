f34bbdd0ce7b8b12beaabadfdc7613a2bdd546b9
Azamat Iskanderov <IskanderovAk98@gmail.com>
2022-12-04 14:51:12 +0300

    add href repo

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,12 +2,18 @@ function Header() {
   return (
     <nav className="red darken-2">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
+        <a href="!#" className="brand-logo">
           React Movies
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="!#">Repo</a>
+            <a
+              href="https://github.com/Azamatik98/MoviesProj"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Repo
+            </a>
           </li>
         </ul>
       </div>
